23ca4f6ce67896ec1e55bb5c78c88b7f8f2f4168
e.nalivkina <e.nalivkina@efko.ru>
2026-08-04 09:19:59 +0300

    wip: корректировка в соответствии с требованиями HttpKernel

--- Http/HttpKernel.php
+++ Http/HttpKernel.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Atlas\Http;
 
 use Atlas\Common\Contract\ErrorHandlerInterface;
-use Atlas\Common\Contract\ModuleInterface;
 use Atlas\Container\ContainerInterface;
+use Atlas\EventDispatcher\Contract\EventDispatcherInterface;
 use Atlas\Http\Contract\HTTPKernelInterface;
 use Atlas\Http\Enum\StatusCode;
 use Atlas\Http\Exceptions\HttpException;
@@ -14,6 +14,7 @@ use Atlas\Http\Exceptions\HttpNotAcceptableException;
 use Atlas\Http\Router\Contract\HttpRouterInterface;
 use Atlas\Logger\Contract\LoggerInterface;
 use Atlas\Http\Contract\ServerResponseInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
@@ -22,14 +23,13 @@ use Psr\Http\Message\ServerRequestInterface;
 final class HttpKernel implements HttpKernelInterface
 {
     public function __construct(
+        private readonly ResponseInterface $response,
         private readonly HttpRouterInterface $router,
         private readonly LoggerInterface $logger,
         private readonly ErrorHandlerInterface $errorHandler,
+        private readonly EventDispatcherInterface $eventDispatcher,
         private readonly ContainerInterface $container,
-        array $modules = [],
-    ) {
-        $this->initModules($modules);
-    }
+    ) {}
 
     public function handle(ServerRequestInterface $request): ServerResponseInterface
     {
@@ -60,17 +60,17 @@ final class HttpKernel implements HttpKernelInterface
                 throw new HttpNotAcceptableException();
             }
 
-            $response = $this->container->get(ServerResponseInterface::class)
+            $response = $this->response
                 ->withStatus($statusCode)
                 ->withHeader('Content-Type', $responseContentType);
 
             $response->getBody()->write((string)$message);
         } catch (HttpException $e) {
-            $this->logger->error($e);
+            $this->logger->error($e->getMessage());
 
             $body = $this->errorHandler->handle($e);
 
-            $response = $this->container->get(ServerResponseInterface::class)
+            $response = $this->response
                 ->withStatus(
                     $e->getStatusCode(),
                     $e->getMessage()
@@ -82,11 +82,27 @@ final class HttpKernel implements HttpKernelInterface
 
             $response->getBody()->write($body);
         } catch (\Throwable $e) {
-            $this->logger->error($e);
+            $this->logger->error($e->getMessage());
 
             $body = $this->errorHandler->handle($e);
 
-            $response = $this->container->get(ServerResponseInterface::class)
+            $response = $this->response
+                ->withStatus(
+                    StatusCode::STATUS_INTERNAL_SERVER_ERROR->value,
+                    $e->getMessage()
+                );
+
+            if ($response->hasHeader('Content-Type') === false) {
+                $response = $response->withHeader('Content-Type', 'text/html; charset=utf-8');
+            }
+
+            $response->getBody()->write($body);
+        } finally {
+            $this->logger->error($e->getMessage());
+
+            $body = $this->errorHandler->handle($e);
+
+            $response = $this->response
                 ->withStatus(
                     StatusCode::STATUS_INTERNAL_SERVER_ERROR->value,
                     $e->getMessage()
@@ -122,22 +138,5 @@ final class HttpKernel implements HttpKernelInterface
         }
 
         return false;
-    }
-
-    /**
-     * Инициализация модулей
-     *
-     * @param array $modules
-     * @return void
-     */
-    private function initModules(array $modules): void
-    {
-        foreach ($modules as $module) {
-            if (is_subclass_of($module, ModuleInterface::class) === false) {
-                throw new \InvalidArgumentException("Модуль {$module} не реализует интерфейс " . ModuleInterface::class);
-            }
-
-            $this->container->call($module, 'init');
-        }
     }
 }
